d5132a5ca3a972dab3c8b61c5f8f5c0b15ba99a4
Corust1411 <sirapob.dar@gmail.com>
2021-03-31 17:31:06 +0700

    * LASTTIME COMMIT * some ui change and some search progress * TODAY COMMIT * Still unfinished search filter :(

--- src/app/components/data-table/CardModel.ts
+++ src/app/components/data-table/CardModel.ts
@@ -6,37 +6,38 @@ import { Observable, of as observableOf, merge } from 'rxjs';
 
 // TODO: Replace this with your own data model type
 export interface DataTableItem {
-  id: number;
-  cardNo: string;
-  firstName: string;
-  lastName: string;
-  deniedList: number;
+      id: number;
+      cardNo: string;
+      firstName: string;
+      lastName: string;
+      deniedList: number;
 }
 
-// TODO: replace this with real data from your application
-const EXAMPLE_DATA: DataTableItem[] = [
-  {id: 1,cardNo: '9079895089465262', firstName: 'Hydrogen',lastName: 'Sodium',deniedList: 1111},
-  {id: 2,cardNo: '9651149919299911', firstName: 'Helium',lastName: 'Magnesium',deniedList: 2222},
-  {id: 3,cardNo: '2858553328316280', firstName: 'Lithium',lastName: 'Aluminum',deniedList: 3333},
-  {id: 4,cardNo: '4904281936053814', firstName: 'Beryllium',lastName: 'Silicon',deniedList: 4444},
-  {id: 5,cardNo: '2614132430115033', firstName: 'Boron',lastName: 'Phosphorus',deniedList: 5555},
-  {id: 6,cardNo: '7409672116264390', firstName: 'Carbon',lastName: 'Sulfur',deniedList: 6666},
-  {id: 7,cardNo: '0071468365538940', firstName: 'Nitrogen',lastName: 'Chlorine',deniedList: 7777},
-  {id: 8,cardNo: '4166937387322085', firstName: 'Oxygen',lastName: 'Argon',deniedList: 8888},
-  {id: 9,cardNo: '6411395029190201', firstName: 'Fluorine',lastName: 'Potassium',deniedList: 9999},
-  {id: 10,cardNo: '5264538285197691', firstName: 'Neon',lastName: 'Calcium',deniedList: 1010},
-  /*{id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},*/
-];
 
+// TODO: replace this with real data from your application
+
+const EXAMPLE_DATA: DataTableItem[] = [
+  {id: 1,cardNo: '9079895089465262', firstName: 'Chanotai',lastName: 'Hydrogen',deniedList: 1111},
+  {id: 2,cardNo: '9651149919299911', firstName: 'Supanut',lastName: 'Helium',deniedList: 2222},
+  {id: 3,cardNo: '2858553328316280', firstName: 'Asma',lastName: 'Lithium',deniedList: 3333},
+  {id: 4,cardNo: '4904281936053814', firstName: 'Pannita',lastName: 'Beryllium',deniedList: 4444},
+  {id: 5,cardNo: '2614132430115033', firstName: 'Napak',lastName: 'Boron',deniedList: 5555},
+  {id: 6,cardNo: '7409672116264390', firstName: 'Paveetida',lastName: 'Carbon',deniedList: 6666},
+  {id: 7,cardNo: '0071468365538940', firstName: 'Pakkapol',lastName: 'Nitrogen',deniedList: 7777},
+  {id: 8,cardNo: '4166937387322085', firstName: 'Sirapob',lastName: 'Oxygen',deniedList: 8888},
+  {id: 9,cardNo: '6411395029190201', firstName: 'Samuel',lastName: 'Fluorine',deniedList: 9999},
+  {id: 10,cardNo: '7859447486990911', firstName: 'Chayatorn',lastName: 'Neon',deniedList: 1010},
+  {id: 11,cardNo: '8458412982333561', firstName: 'Nuntapob',lastName: 'Sodium',deniedList: 1111 },
+  {id: 12,cardNo: '5351034826075236', firstName: 'Korwies',lastName: 'Magnesium',deniedList: 1212},
+  {id: 13,cardNo: '6312107661841406', firstName: 'Puttipong',lastName: 'Aluminum',deniedList: 1313},
+  {id: 14,cardNo: '0984764707406605', firstName: 'Nuddcha',lastName: 'Silicon',deniedList: 1414},
+  {id: 15,cardNo: '5244082811409330', firstName: 'Komas',lastName: 'Phosphorus',deniedList: 1515},
+  {id: 16,cardNo: '4561198320082507', firstName: 'Wichayada',lastName: 'Sulfur',deniedList: 1616},
+  {id: 17,cardNo: '9534306652538602', firstName: 'Chatchakorn',lastName: 'Chlorine',deniedList: 1717},
+  {id: 18,cardNo: '1850528689508200', firstName: 'Pipatchai',lastName: 'Argon',deniedList: 1818},
+  {id: 19,cardNo: '0414159684062747', firstName: 'Pakin',lastName: 'Potassium',deniedList: 1919},
+  {id: 20,cardNo: '5557630083949963', firstName: 'Complex',lastName: 'Calcium',deniedList: 2020},
+];
 /**
  * Data source for the DataTable view. This class should
  * encapsulate all logic for fetching and manipulating the displayed data
